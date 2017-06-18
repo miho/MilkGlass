a020637be66e747ea8830fb03d9c2a42d2851184
Michael Hoffer <info@michaelhoffer.de>
2017-06-19 01:01:10 +0200

    License info added, updated Gradle version, Fix for issue #2

--- src/main/java/eu/mihosoft/fx/tutorials/milkglass/MilkGlassPane.java
+++ src/main/java/eu/mihosoft/fx/tutorials/milkglass/MilkGlassPane.java
@@ -1,8 +1,31 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2017 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Michael Hoffer <info@michaelhoffer.de> "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Michael Hoffer <info@michaelhoffer.de> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Michael Hoffer <info@michaelhoffer.de>.
  */
+
 package eu.mihosoft.fx.tutorials.milkglass;
 
 import javafx.animation.AnimationTimer;
@@ -70,8 +93,8 @@ public class MilkGlassPane extends Region {
     private void updateBackground() {
 
         // return null if this pane is invisible
-        if (getWidth() <= 0
-                || getHeight() <= 0
+        if (getWidth() < 1
+                || getHeight() < 1
                 || this.getOpacity() == 0) {
             return;
         }
@@ -106,7 +129,7 @@ public class MilkGlassPane extends Region {
                 getHeight());
         sp.setViewport(rect);
 
-        // create the snaphot
+        // create the snapshot
         container.snapshot(sp, image);
 
     }
